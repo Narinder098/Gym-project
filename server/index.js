814b41d7f3ec195ec299af9d05f59fb110ec41f9
Narinder098 <Narindersuthar16@email.com>
2025-06-22 00:50:11 +0530

    improved the core funtionality

--- server/index.js
+++ server/index.js
@@ -15,10 +15,13 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  // origin: "http://localhost:5173", 
-  origin:"https://sparkly-swan-106dd9.netlify.app",
-  credentials: true,               
+  origin: [
+    "http://localhost:5173",
+    "https://sparkly-swan-106dd9.netlify.app"
+  ],
+  credentials: true
 }));
+
 
 connectDB();
 
